0e72db74e11b03c299a76c65b85e29487a20a30f
Salma Azzahra Salsabila <salma.2000@students.amikom.ac.id>
2019-04-13 08:34:15 +0700

    constructor

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            cons objTime = new cons(2000,3,7,14,50,20);
+
+            // mengambil nilai dengan property
+            Console.WriteLine("Year : {0}", objTime.Year.ToString());
+            Console.WriteLine("Month : {0}", objTime.Month.ToString());
+            Console.WriteLine("Date : {0}", objTime.Date);
+            Console.WriteLine("Hour : {0}", objTime.Hour);
+            Console.WriteLine("Minute : {0}", objTime.Minute);
+            Console.WriteLine("Second : {0}", objTime.Second);
+            Console.ReadKey();
+        }
+        
+    }
+}
---
+++ cons.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class cons
+    {
+        // private variables atau field
+        private int year;
+        private int month;
+        private int date;
+        private int hour;
+        private int minute;
+        private int second;
+        // property
+        public int Year {
+            get { return year; }
+            set { year = value; }
+        }
+        public int Month {
+            get { return month; }
+            set { month = value; }
+        }
+        public int Date {
+            get { return date; }
+            set { date = value; }
+        }
+        public int Hour {
+            get { return hour; }
+            set { hour = value; }
+        }
+        public int Minute {
+            get { return minute; }
+            set { minute = value; }
+        }
+        public int Second {
+            get { return second; }
+            set { second = value; }
+        }
+
+        // constructor
+        public cons(int year, int month, int date,
+            int hour, int minute, int second)
+        {
+            this.year = year;
+            this.month = month;
+            this.date = date;
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;
+        }
+    }
+}
